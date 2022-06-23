67505af18666ab8685b5a214ff5544ef4020f49c
anujaios <97096428+anujaios@users.noreply.github.com>
2022-06-23 11:20:03 +0530

    initial commit

---
+++ searchBar/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  searchBar
+//
+//  Created by Ashlesha Kamble on 20/06/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var secondtableview: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- searchBar/ViewController.swift
+++ searchBar/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var theSearch: UISearchBar!
+    @IBOutlet weak var firstTableview: UITableView!
+    
+    var searchActive = true
+    var countriesArray = ["Dominica","France","Germany","Japan","Albania","India","Cuba","Bolivia","Latvia","Morocco","Armenia"]
+    //var capitalsArray = ["Roseau","Paris","Berlin","Tokyo","Tirana","Delhi","Havana","Sucre","Riga","Rabat"]
+    var filteredArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstTableview.dataSource = self
+        firstTableview.delegate = self
+        theSearch.delegate = self
+        filteredArray = countriesArray
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate,UITableViewDataSource, UISearchBarDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (searchActive) {
+            return filteredArray.count
+        } else {
+            return countriesArray.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if filteredArray.count != 0 {
+            cell.textLabel?.text = filteredArray[indexPath.row]
+        } else {
+            cell.textLabel?.text = countriesArray[indexPath.row]
+        }
+
+            return cell
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+         filteredArray = []
+        if searchText == "" {
+            filteredArray = countriesArray
+        }
+        else {
+            for skills in countriesArray {
+                if skills.lowercased().contains(searchText.lowercased()) {
+                    filteredArray.append(skills)
+                }
+            }
+        }
+        self.firstTableview.reloadData()
+    
+}
+
+}
